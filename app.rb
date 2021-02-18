0be808e7a471c97f65c9c1a7a392ecf2f6d6959b
Jimjule <roadknightj@gmail.com>
2021-02-18 17:56:39 +0000

    Adds web_in_out class

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require('sinatra')
 require('shotgun')
+require('tictactoe_jules')
+require './lib/web_in_out'
 
 get '/' do
   erb :index
---
+++ spec/web_in_out_spec.rb
@@ -0,0 +1,29 @@
+require 'web_in_out'
+
+describe Web_in_out do
+  let(:web_in_out) { Web_in_out.new(nil, nil) }
+
+  it 'Has a print method' do
+    expect(web_in_out).to respond_to(:print).with(1).arguments
+  end
+
+  it 'Has a get play against computer method' do
+    expect(web_in_out).to respond_to(:get_play_against_computer)
+  end
+
+  it 'Has a set board size method' do
+    expect(web_in_out).to respond_to(:set_board_size)
+  end
+
+  it 'Has a get player name method' do
+    expect(web_in_out).to respond_to(:get_player_name).with(1).arguments
+  end
+
+  it 'Has a select move method' do
+    expect(web_in_out).to respond_to(:select_move)
+  end
+
+  it 'Has a clear method' do
+    expect(web_in_out).to respond_to(:clear)
+  end
+end

2d0b2f9aecc476b92ba0e14f0ff5900dcfd2653d
Jimjule <roadknightj@gmail.com>
2021-02-22 14:34:38 +0000

    Adds game setup form to page view

--- cypress/integration/starting_a_game.spec.js
+++ cypress/integration/starting_a_game.spec.js
@@ -8,7 +8,35 @@ describe('Starting a game', () => {
     cy.url()
       .should('include', '/game_setup')
 
-    cy.get('[data-testid="setup_greeting"')
+    cy.get('[data-testid="setup_greeting"]')
       .should('be.visible')
+
+    cy.get('[data-testid="player_1_name"]')
+      .type('X')
+
+    cy.get('[data-testid="setup_greeting"]')
+    .should('be.visible')
+
+    cy.get('[data-testid="player_2_name"]')
+      .should('be.disabled')
+
+    cy.get('[type="checkbox"]')
+      .check()
+
+    cy.get('[data-testid="player_2_is_computer"]')
+      .should('be.checked')
+
+    cy.get('[data-testid="player_2_name"]')
+      .should('not.be.disabled')
+
+    cy.get('[data-testid="board_size"]')
+      .should('have.value', '3')
+
+    cy.get('[data-testid="board_size"]')
+      .clear()
+      .type(4)
+
+    cy.get('[data-testid="board_size"]')
+      .should('have.value', '4')
   })
 })

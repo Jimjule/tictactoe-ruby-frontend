9e43a77856c0699a3019d67a161758017b9e0553
Jimjule <roadknightj@gmail.com>
2021-02-23 11:48:56 +0000

    Test drives game setup features

--- cypress/integration/starting_a_game.spec.js
+++ cypress/integration/starting_a_game.spec.js
@@ -1,5 +1,5 @@
 describe('Starting a game', () => {
-  it('Goes to the game setup page', () => {
+  it('Cannot submit form without required fields', () => {
     cy.visit('http://localhost:4567')
 
     cy.get('[data-testid="game_setup"]')
@@ -11,11 +11,51 @@ describe('Starting a game', () => {
     cy.get('[data-testid="setup_greeting"]')
       .should('be.visible')
 
+    cy.get('[data-testid="player_2_name"]')
+      .should('be.disabled')
+
+    cy.get('[data-testid="player_2_is_computer"]')
+      .should('be.checked')
+
+    cy.get('[data-testid="board_size"]')
+      .should('have.value', '3')
+
+    cy.get('[type="submit"]')
+      .click()
+
+    cy.url()
+      .should('equal', 'http://localhost:4567/game_setup?')
+
     cy.get('[data-testid="player_1_name"]')
       .type('X')
 
-    cy.get('[data-testid="setup_greeting"]')
-      .should('be.visible')
+    cy.get('[data-testid="board_size"]')
+      .clear()
+
+    cy.get('[type="submit"]')
+      .click()
+
+    cy.url()
+      .should('equal', 'http://localhost:4567/game_setup?')
+
+    cy.get('[data-testid="board_size"]')
+      .type('3')
+
+    cy.get('[type="checkbox"]')
+      .uncheck()
+
+    cy.get('[data-testid="player_2_name"]')
+      .clear()
+
+    cy.get('[type="submit"]')
+      .click()
+
+    cy.url()
+      .should('equal', 'http://localhost:4567/game_setup?')
+  })
+
+  it('Resets Player 2 name to Computer if computer player chosen', () => {
+    cy.visit('http://localhost:4567/game_setup')
 
     cy.get('[data-testid="player_2_name"]')
       .should('be.disabled')
@@ -23,11 +63,14 @@ describe('Starting a game', () => {
     cy.get('[type="checkbox"]')
       .check()
 
+    cy.get('[data-testid="player_2_name"]')
+      .should('be.disabled')
+
     cy.get('[data-testid="player_2_is_computer"]')
       .should('be.checked')
 
     cy.get('[data-testid="player_2_name"]')
-      .should('be.disabled')
+      .should('have.value', 'Computer')
 
     cy.get('[type="checkbox"]')
       .uncheck()
@@ -36,10 +79,35 @@ describe('Starting a game', () => {
       .should('not.be.checked')
 
     cy.get('[data-testid="player_2_name"]')
+      .clear()
+
+    cy.get('[data-testid="player_2_name"]')
       .type('O')
 
-    cy.get('[data-testid="board_size"]')
-      .should('have.value', '3')
+    cy.get('[type="checkbox"]')
+      .check()
+
+    cy.get('[data-testid="player_2_name"]')
+      .should('have.value', 'Computer')
+  })
+
+  it('Submits game setup values', () => {
+    cy.visit('http://localhost:4567')
+
+    cy.get('[data-testid="game_setup"]')
+      .click();
+
+    cy.get('[data-testid="player_1_name"]')
+      .type('X')
+
+    cy.get('[type="checkbox"]')
+      .uncheck()
+
+    cy.get('[data-testid="player_2_name"]')
+      .clear()
+
+    cy.get('[data-testid="player_2_name"]')
+      .type('O')
 
     cy.get('[type="submit"]')
       .should('not.be.disabled')

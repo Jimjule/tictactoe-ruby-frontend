724ef541867706e3f4ce65f613950eebabf87210
Jimjule <roadknightj@gmail.com>
2021-03-03 13:01:49 +0000

    Tests that a 4x4 board displays correctly

--- cypress/integration/board.spec.js
+++ cypress/integration/board.spec.js
@@ -31,6 +31,38 @@ describe('Starting a game', () => {
       .contains('Computer\'s Move')
   })
 
+  it('Displays and marks a 4 x 4 board', () => {
+    cy.setupGameWithComputer('Player 1', 4)
+
+    cy.get('[data-testid="board_display"]')
+      .should('be.visible')
+
+    cy.get('[data-testid="board_display"]')
+      .contains('1 2 3 4')
+    cy.get('[data-testid="board_display"]')
+      .contains('5 6 7 8')
+    cy.get('[data-testid="board_display"]')
+      .contains('9 10 11 12')
+
+    cy.get('[data-testid="turn_counter"')
+      .contains('1')
+
+    cy.get('[data-testid="whose_move"]')
+      .contains('Player 1\'s Move')
+
+    cy.get('[data-testid="enter_move"]')
+      .type('16')
+
+    cy.get('[data-testid="submit_move"]')
+      .click()
+
+    cy.get('[data-testid="turn_counter"')
+      .contains('2')
+
+    cy.get('[data-testid="whose_move"]')
+      .contains('Computer\'s Move')
+  })
+
   it('Marks the board when a move is made', () => {
     cy.setupGameWithPlayer('Player 1', 'Player 2', 3)
 
